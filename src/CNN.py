59041df419c4e331db9813637d7707269ec8d040
Samira Abnar <samiraabnar@gmail.com>
2016-03-07 15:23:52 +0100

    To Synchronize

--- src/CNN.py
+++ src/CNN.py
@@ -2,10 +2,12 @@ import theano
 import theano.tensor as T
 import numpy as np
 from theano.tensor.nnet import conv
+from theano.tensor.signal import downsample
+
 
 class CNN(object):
 
-    def __init__(self, input_shape= (1,96,96),number_of_feature_maps_1,number_of_feature_maps_2,filter_shape)
+    def __init__(self, input_shape= (1,96,96),number_of_feature_maps_1,number_of_feature_maps_2,filter_shape, pooling_size=(2, 2))
 
         """
         input: a 4D tensor corresponding to a mini-batch of input images. The shape of the tensor is as follows:
@@ -28,3 +30,40 @@ class CNN(object):
         #Colvolution
 
         self.convolution = conv.conv2d(self.input,self.W)
+        self.max_pooling = downsample.max_pool_2d(
+            input=self.convolution,
+            ds=pooling_size,
+            ignore_border=True
+        )
+
+        output = T.tanh(self.convolution + self.bias.dimshuffle('x', 0, 'x', 'x'))
+
+        f = theano.function([input], output)
+
+
+    def build(self):
+
+        x = T.matrix('x')
+        y = T.ivector('y')
+
+
+        input_layer = x.reshape((batch_size, 1, self.image_shape[0], self.image_shape[1]))
+
+        layers = {}
+        layers[0] = LeNetConvPoolLayer(
+                    rng,
+                    input=input_layer,
+                    image_shape= self.image_shape,
+                    filter_shape = self.filter_shape,
+                    pooling_size = self.pooling_size
+
+        )
+
+        layers[1] = LeNetConvPoolLayer(
+                    rng,
+                    input=layers[0].output,
+                    image_shape= self.image_shape,
+                    filter_shape = self.filter_shape,
+                    pooling_size = self.pooling_size
+
+        )
